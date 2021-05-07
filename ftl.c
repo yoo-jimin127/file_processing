8e4bad73914bd4b2a813232c800c83168a91475b
yoo-jimin127 <dbwlals9936@naver.com>
2021-05-08 02:23:50 +0900

    21.05.08 2:23

--- ftl.c
+++ ftl.c
@@ -30,8 +30,8 @@ typedef struct {
 }AddrMappingTable;
 
 AddrMappingTable addrMappingTable; //address mapping table 구조체 변수
-int freeBlockIdx; //free block의 pbn 값의 초기화를 위해 필요한 인덱스 변수
-
+int freeBlockIdx = 0; //free block의 pbn 값의 초기화를 위해 필요한 인덱스 변수
+int pbnIdx = 0; //ftl_write()에서 pbn 값을 임의로 지정하여줄 때 필요한 변수
 //
 // flash memory를 처음 사용할 때 필요한 초기화 작업, 예를 들면 address mapping table에 대한
 // 초기화 등의 작업을 수행한다. 따라서, 첫 번째 ftl_write() 또는 ftl_read()가 호출되기 전에
@@ -104,25 +104,77 @@ void ftl_read(int lsn, char *sectorbuf) {
 //
 void ftl_write(int lsn, char *sectorbuf) {
 	char spare_data[SPARE_SIZE]; //spare영역의 메타데이터 작성을 위한 배열
+	char cmp_spare_data[SPARE_SIZE]; //임시 스페어 버퍼
+	char tmpbuf[PAGE_SIZE]; //임시 버퍼
 	char pagebuf[PAGE_SIZE]; //페이지 버퍼
 	int lbn, offset; //데이터를 쓸 lbn과 offset
-	int pbn;
-	int ppn;
+	int pbn;//physical block number
+	int ppn; //physical page number
+	int i = 0, j = 0, k = 0, check = 0; 
 	
-	memset(spare_data, 0, SPARE_SIZE);
-	memset(pagebuf, 0, PAGE_SIZE);
+	memset(spare_data, 0, SPARE_SIZE); //스페어버퍼 초기화
+	memset(cmp_spare_data, 0, SPARE_SIZE); //임시 스페어버퍼 초기화
+	memset(tmpbuf, 0, PAGE_SIZE); //임시버퍼 초기화
+	memset(pagebuf, 0, PAGE_SIZE); //페이지버퍼 초기화
+	
+	memcpy(pagebuf, sectorbuf, SECTOR_SIZE); //임시버퍼에 섹터버퍼 내용 옮김
 
 	lbn = lsn / PAGES_PER_BLOCK; //데이터를 쓸 블록의 번호를 얻기 위해 lsn/블럭 개수
 	offset = lsn % PAGES_PER_BLOCK; //lbn번째 블록의 몇번째 페이지에 데이터를 쓸 것인지에 대한 offset
-	ppn = addrMappingTable.entry[lsn].pbn;
+	
+	if (addrMappingTable.entry[lbn].pbn == -1) {
+		addrMappingTable.entry[lbn].pbn = pbnIdx++;
 
-	if (ppn == -1) { //lsn에 최초로 데이터를 쓰는 경우
+	pbn = addrMappingTable.entry[lbn].pbn; //pbn에 값 저장
+	ppn = (pbn * PAGES_PER_BLOCK) + offset; //페이지 넘버 : pbn * 블록별 페이지 개수 + offset
+
+	sprintf(spare_data, "%d%d", lbn, lsn); //스페어 버퍼에 lbn, lsn 씀
+	memcpy(pagebuf + SECTOR_SIZE, spare_data, SPARE_SIZE);
+	
+	dd_read(ppn, tmpbuf); //tmpbuf에 해당 ppn의 데이터 읽어오기
+
+	if (strncmp(tmpbuf + SECTOR_SIZE, spare_data, strlen(spare_data)) != 0) { //스페어데이터와 tmpbuf의 데이터 다른 경우
+		for (i = PAGES_PER_BLOCK + (PAGES_PER_BLOCK * pbn); i < PAGES_PER_BLOCK + (PAGES_PER_BLOCK * pbn); i++) {
+			dd_read(i, tmpbuf); //해당 ppn의 내용을 tmpbuf에 읽어옴
+			
+			if (isdigit(*(tmpbuf + SECTOR_SIZE))) { //숫자 판단 함수를 사용해 spare area에 숫자가 존재하면
+				continue;
+
+			}
+
+			dd_write(i, pagebuf); //pagebuf의 내용을 i번째 ppn에 적음
+			break;
+		}
+
+		if (PAGES_PER_BLOCK + (pbn * PAGES_PER_BLOCK) == i) { //i의 ppn이 입력받은 ppn과 같은 경우
+			for (j = PAGES_PER_BLOCK * pbn, check = 0; j < PAGES_PER_BLOCK + (PAGES_PER_BLOCK * pbn); j++, check++) {
+				if (ppn = j) {
+					continue;
+				}
+
+				for (k = (PAGES_PER_BLOCK * pbn) + PAGES_PER_BLOCK; k > PAGES_PER_BLOCK + (PAGES_PER_BLOCK * pbn); k--) {
+					sprintf(cmp_spare_data, "%d", k); //k값을 cmp_spare_data에 저장
+					dd_read(k, tmpbuf); //dd_read로 k번째 ppn의 값을 읽어옴
+
+					if (strcmp(tmpbuf + SECTOR_SIZE, cmp_spare_data) != 0) {
+						dd_write();
+						break;
+					}
+				}
+
+				if ()
+			}
+
+		}
+	}
+
+	/*if (pbn == -1) { //lsn에 최초로 데이터를 쓰는 경우
 		
 	}
 
 	else { //lsn에 데이터를 갱신(update)하는 경우
 
-	}
+	} */
 
 	return;
 }
